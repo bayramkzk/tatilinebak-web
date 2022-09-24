1f85137c587deeff27a60af5913ec63808bc7042
highberg <48856944+highberg@users.noreply.github.com>
2022-09-24 22:36:39 +0300

    Customize mantine theme with dark mode shortcut

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,62 @@
 import "@/styles/globals.css";
-import { MantineProvider } from "@mantine/core";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  MantineThemeOverride,
+} from "@mantine/core";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
 import { NotificationsProvider } from "@mantine/notifications";
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
+export default function App({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+  const mantineTheme: MantineThemeOverride = {
+    colorScheme,
+    colors: {
+      brandPrimary: [
+        "#fbe9e7",
+        "#ffccbc",
+        "#ffab91",
+        "#ff8a65",
+        "#ff7043",
+        "#ff5722",
+        "#f4511e",
+        "#e64a19",
+        "#d84315",
+        "#bf360c",
+      ],
+      brandSecondary: [
+        "#fffde7",
+        "#fff9c4",
+        "#fff59d",
+        "#fff176",
+        "#ffee58",
+        "#ffeb3b",
+        "#fdd835",
+        "#fbc02d",
+        "#f9a825",
+        "#f57f17",
+      ],
+      brandGradient:
+        colorScheme === "dark"
+          ? ["#ce2e3c", "#ce9240"]
+          : ["#ee4e5c", "#eeb260"],
+    },
+    primaryColor: "brandPrimary",
+    primaryShade: { light: 6, dark: 8 },
+  };
 
   return (
     <>
@@ -15,17 +66,19 @@ export default function App(props: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{ colorScheme: "light" }}
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <NotificationsProvider>
-          <Component {...pageProps} />
-        </NotificationsProvider>
-      </MantineProvider>
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={mantineTheme}>
+          <NotificationsProvider>
+            <Component {...pageProps} />
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
