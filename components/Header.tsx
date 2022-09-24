447de87b37f26f06d1e505f66152df230c2b495e
highberg <48856944+highberg@users.noreply.github.com>
2022-09-25 01:11:22 +0300

    Implement main and social links for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,8 +8,10 @@ import {
   Transition,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { useState } from "react";
+import Link from "next/link";
 import BrandLogo from "./BrandLogo";
+import LinkGroup from "./LinkGroup";
+import SocialLinkGroup from "./SocialLinkGroup";
 
 const HEADER_HEIGHT = 75;
 
@@ -17,6 +19,7 @@ const useStyles = createStyles((theme) => ({
   root: {
     position: "relative",
     zIndex: 1,
+    borderBottomWidth: 2,
   },
 
   dropdown: {
@@ -54,93 +57,61 @@ const useStyles = createStyles((theme) => ({
     },
   },
 
-  link: {
-    display: "block",
-    lineHeight: 1,
-    padding: "12px 16px",
-    borderRadius: theme.radius.sm,
-    textDecoration: "none",
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-
-    [theme.fn.smallerThan("sm")]: {
-      borderRadius: 0,
-      padding: theme.spacing.md,
-    },
-  },
-
-  linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.fn.variant({
-        variant: "light",
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
-        .color,
+  burgerSocials: {
+    padding: theme.spacing.md,
+    justifyContent: "space-evenly",
+    "& > *": {
+      flexGrow: 1,
+      display: "flex",
+      justifyContent: "center",
     },
   },
 }));
 
+const mainLinks = (
+  <LinkGroup
+    links={[
+      { link: "/turlar", content: "Turlar" },
+      { link: "/oteller", content: "Oteller" },
+    ]}
+  />
+);
+
+const asideLinks = (
+  <LinkGroup
+    links={[
+      { link: "/hakkimizda", content: "Hakkımızda" },
+      { link: "/iletisim", content: "İletişim" },
+    ]}
+  />
+);
+
+const socialLinks = <SocialLinkGroup />;
+
 export interface HeaderProps {}
 
-const links = [
-  {
-    link: "/about",
-    label: "Features",
-  },
-  {
-    link: "/pricing",
-    label: "Pricing",
-  },
-  {
-    link: "/learn",
-    label: "Learn",
-  },
-  {
-    link: "/community",
-    label: "Community",
-  },
-];
-
 const Header: React.FC<HeaderProps> = () => {
-  const [opened, { toggle, close }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
+  const [opened, { toggle }] = useDisclosure(false);
   const { classes, cx } = useStyles();
-
-  const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={cx(classes.link, {
-        [classes.linkActive]: active === link.link,
-      })}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-        close();
-      }}
-    >
-      {link.label}
-    </a>
-  ));
 
   return (
     <BaseHeader height={HEADER_HEIGHT} className={classes.root}>
       <Container size="xl" className={classes.header}>
-        <BrandLogo size={20} />
-        <Group spacing={5} className={classes.links}>
-          {items}
+        <Group>
+          <Link href="/" passHref>
+            <a>
+              <BrandLogo size={20} />
+            </a>
+          </Link>
+
+          <Group spacing={5} className={classes.links}>
+            {mainLinks}
+          </Group>
+        </Group>
+
+        <Group className={classes.links}>
+          <Group spacing={5}>{asideLinks}</Group>
+          <Group spacing={2}>{socialLinks}</Group>
         </Group>
 
         <Burger
@@ -153,7 +124,8 @@ const Header: React.FC<HeaderProps> = () => {
         <Transition transition="pop-top-right" duration={200} mounted={opened}>
           {(styles) => (
             <Paper className={classes.dropdown} withBorder style={styles}>
-              {items}
+              {mainLinks} {asideLinks}
+              <Group className={classes.burgerSocials}>{socialLinks}</Group>
             </Paper>
           )}
         </Transition>
