8d443304d4eb776aac98ed588b91a2cd4b39e448
highberg <48856944+highberg@users.noreply.github.com>
2022-09-25 01:37:37 +0300

    Wrap main content with container in layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,11 @@
-import { createStyles } from "@mantine/core";
+import { Container, createStyles, MantineNumberSize } from "@mantine/core";
 import React from "react";
 import Header from "./Header";
 
 const useStyles = createStyles((themes) => ({
+  container: {
+    width: "100%",
+  },
   main: {
     width: "100%",
     height: "100%",
@@ -11,15 +14,18 @@ const useStyles = createStyles((themes) => ({
 
 interface LayoutProps {
   children: React.ReactNode;
+  size?: MantineNumberSize;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, size = "xl" }) => {
   const { classes } = useStyles();
 
   return (
     <>
-      <Header />
-      <main className={classes.main}>{children}</main>
+      <Header size={size} mb="lg" />
+      <Container size={size} className={classes.container}>
+        <main className={classes.main}>{children}</main>
+      </Container>
     </>
   );
 };
