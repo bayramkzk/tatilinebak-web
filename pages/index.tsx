a12c9afc2d9d5476f28ceb88f161df74cfc9940f
highberg <48856944+highberg@users.noreply.github.com>
2022-09-24 15:34:14 +0300

    Set up mantine ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
+import { Button, Center, Modal, Stack } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  return <div>hello world</div>;
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => setModal(true);
+  const closeModal = () => setModal(false);
+  const pushNotification = () =>
+    showNotification({
+      title: "Default notification",
+      message: "Hey there, your code is awesome! 🤥",
+    });
+
+  return (
+    <Center sx={{ width: "100%", height: "100%" }}>
+      <Stack>
+        <Button onClick={openModal} color="blue">
+          Show dialog!
+        </Button>
+        <Button onClick={pushNotification} color="lime">
+          Push notification
+        </Button>
+      </Stack>
+
+      <Modal onClose={closeModal} opened={modal}>
+        Hello world
+      </Modal>
+    </Center>
+  );
 };
 
 export default Home;
